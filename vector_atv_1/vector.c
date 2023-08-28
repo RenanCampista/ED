8c8115dcdcacb354a835f9238a80e707a46f7368
Renan Campista <renan.campistah@hotmail.com>
2023-08-28 11:42:41 -0300

    finalizar na aula

--- vector_atv_1/vector.c
+++ vector_atv_1/vector.c
@@ -61,3 +61,96 @@ int vector_find(Vector *v, data_type val) {
     }
     return -1;
 }
+
+data_type vector_remove(Vector *v, int i) {
+    if (i >= 0 && i < v->size) {
+        data_type val = v->data[i];
+        for (int j = i; j < v->size - 1; j++) {
+            v->data[j] = v->data[j + 1];
+        }
+        v->size--;
+        return val;
+    }
+    printf("O indice informado eh negativo ou maior que o tamanho do vetor");
+    exit(1);
+}
+
+data_type vector_pop_front(Vector *v) {
+    return vector_remove(v, 0);
+}
+
+data_type vector_pop_back(Vector *v) {
+    return vector_remove(v, v->size - 1);
+}
+
+Vector *vector_copy(Vector *v) {
+    Vector *copy = vector_construct();
+    for (int i = 0; i < v->size; i++) {
+        vector_push_back(copy, v->data[i]);
+    }
+    return copy;
+}
+
+void vector_insert(Vector *v, int i, data_type val) {
+    if (v->size >= v->allocated) {
+        v->allocated += 10;
+        v->data = (data_type *) realloc(v->data, v->allocated * sizeof(data_type));
+        if (v == NULL) {
+            printf("Memoria insuficiente para aumentar o tamanho do vetor.\n");
+            exit(1);
+        }
+    }
+
+    Vector *copy = vector_copy(v);
+
+    for (int j = i; j < v->size; j++) {
+        v->data[j+1] = copy->data[j];
+    }
+    v->data[i] = val;
+    v->size++;
+
+    vector_destroy(copy);
+}
+
+void vector_swap(Vector *v, int i, int j) {
+    data_type copy = v->data[i];
+    v->data[i] = v->data[j];
+    v->data[j] = copy;
+}
+
+void vector_sort(Vector *v) {
+    for (int i = 0; i < v->size - 1; i++) {
+        for (int j = i + 1; j < v->size; j++) {
+            if (v->data[i] > v->data[j]) {
+                vector_swap(v, i, j);
+            }
+        }
+    }
+}
+
+/*int vector_binary_search(Vector *v, data_type val) {
+    vector_sort(v);
+    int left = 0;
+    int right = v->size - 1;
+    while (left <= right) {
+        int middle = (left + right) / 2;
+        if (v->data[middle] == val) {
+            return middle;
+        } else if (v->data[middle] < val) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return -1;
+}*/
+
+void vector_reverse(Vector *v) {
+    for (int i = 0; i < v->size - 1; i++) {
+        for (int j = i + 1; j < v->size; j++) {
+
+            vector_swap(v, i, j);
+            
+        }
+    }
+}
